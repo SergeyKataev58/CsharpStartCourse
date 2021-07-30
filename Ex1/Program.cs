4e69a48d4f773e9df6831da96b0d568c86ed2f40
Сергей <@DESKTOP-5JQRDG9>
2021-07-30 08:30:46 +0300

    3

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -27,7 +27,7 @@ namespace Ex1     // Задача 1 (Анкета) Катаев Сергей
               Console.WriteLine("{0:G}", name + " " + fename + " " + age + " " + rost + " " + ves); //вариант b
 
               Console.WriteLine(string.Format($"{name} {fename} {age} {rost} {ves}")); //вариант с
-            */
+            
             // Задача 2 (найти ИМТ человека)
             Console.WriteLine("Задача 2 (найти ИМТ человека)");
             Console.Write("Введите ваш рост в метрах:");
@@ -43,8 +43,33 @@ namespace Ex1     // Задача 1 (Анкета) Катаев Сергей
             double imt = b / (a * a);
 
             Console.WriteLine("Индекс массы вашего тела:" + imt);
+            */
+            //Задача 3 (Посчитать расстояние между точками)
 
+            //Ввод координат
+            Console.Write("Введите x1 = ");
+            var x1 = Console.ReadLine();
 
+            double a1 = Convert.ToDouble(x1);
+
+            Console.Write("Введите x2 = ");
+            var x2 = Console.ReadLine();
+
+            double a2 = Convert.ToDouble(x2);
+
+            Console.Write("Введите y1 = ");
+            var y1 = Console.ReadLine();
+
+            double b1 = Convert.ToDouble(y1);
+
+            Console.Write("Введите y2 = ");
+            var y2 = Console.ReadLine();
+
+            double b2 = Convert.ToDouble(y2);
+
+            double r = Math.Sqrt(Math.Pow(a2 - a1, 2) + Math.Pow(b2 - b1, 2));
+
+            Console.WriteLine("Расстояние между точками = " + "{0:F2}",r);
 
         }
 
